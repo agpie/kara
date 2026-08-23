88b5fd9fb79c0a4047a92be90bfb97bbd164532b
Maxwell Johnson <69218421+agpie@users.noreply.github.com>
2026-08-23 11:09:44 +0100

    Add initial songs data structure

---
+++ songs.js
@@ -0,0 +1,19 @@
+var SONGS = [
+      {
+        title: 'Polaroid Love',
+        artist: 'ENHYPEN',
+        lyricId: 'zVg4HIFVQ2U',
+        lyricStart: 2.0,
+        instId: '7PvV7EgO__0',
+        instStart: 0.5
+      },
+      {
+        title: '7 Rings',
+        artist: 'Ariana Grande',
+        lyricId: 'uDAjINEp8H8',
+        lyricStart: 0.0,
+        instId: 'M84fFXooS5w',
+        instStart: 0.0
+      }
+      // add more songs here
+    ];
